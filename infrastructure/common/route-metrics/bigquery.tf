9d7558d0b81317b3a625391c0c1094c47f50fc7e
Bianca Berdugo <bianca_berdugo@hotmail.com>
2025-03-22 17:18:46 +0100

    add simulation

--- infrastructure/common/route-metrics/bigquery.tf
+++ infrastructure/common/route-metrics/bigquery.tf
@@ -1,15 +1,22 @@
 resource "google_bigquery_table" "route-metrics" {
-  table_id   = "route-metrics"
+  table_id   = "route_metrics"
   dataset_id = var.bigquery-dataset-id-lading-layer
 
   schema = <<EOF
 [
-  {
-    "name": "data",
-    "type": "STRING",
-    "mode": "NULLABLE",
-    "description": "The data"
-  }
+  {"name": "event_id",         "type": "STRING",  "mode": "NULLABLE"},
+  {"name": "car_id",           "type": "STRING",  "mode": "NULLABLE"},
+  {"name": "trip_id",          "type": "STRING",  "mode": "NULLABLE"},
+  {"name": "timestamp",        "type": "FLOAT",   "mode": "NULLABLE"},
+  {"name": "latitude",         "type": "FLOAT",   "mode": "NULLABLE"},
+  {"name": "longitude",        "type": "FLOAT",   "mode": "NULLABLE"},
+  {"name": "latitude_start",   "type": "FLOAT",   "mode": "NULLABLE"},
+  {"name": "longitude_start",  "type": "FLOAT",   "mode": "NULLABLE"},
+  {"name": "latitude_end",     "type": "FLOAT",   "mode": "NULLABLE"},
+  {"name": "longitude_end",    "type": "FLOAT",   "mode": "NULLABLE"},
+  {"name": "city_id",          "type": "INTEGER", "mode": "NULLABLE"},
+  {"name": "user_id",          "type": "STRING",  "mode": "NULLABLE"},
+  {"name": "driver_id",        "type": "STRING",  "mode": "NULLABLE"}
 ]
 EOF
 
--- infrastructure/common/route-metrics/pubsub.tf
+++ infrastructure/common/route-metrics/pubsub.tf
@@ -1,6 +1,45 @@
+resource "google_pubsub_schema" "app-route-metrics-topic-schema" {
+  name       = "gps-car-trip-simulation-schema"
+  type       = "AVRO"
+  definition = <<EOF
+{
+  "type": "record",
+  "name": "GPSCarTripSimulation",
+  "namespace": "com.example.avro",
+  "fields": [
+    {"name": "event_id",         "type": "string"},
+    {"name": "car_id",           "type": "string"},
+    {"name": "trip_id",          "type": "string"},
+    {"name": "timestamp",        "type": "double"},
+    {"name": "latitude",         "type": "double"},
+    {"name": "longitude",        "type": "double"},
+    {"name": "latitude_start",   "type": "double"},
+    {"name": "longitude_start",  "type": "double"},
+    {"name": "latitude_end",     "type": "double"},
+    {"name": "longitude_end",    "type": "double"},
+    {"name": "city_id",          "type": "int"},
+    {"name": "user_id",          "type": "string"},
+    {"name": "driver_id",        "type": "string"}
+  ]
+}
+EOF
+}
+
+
 resource "google_pubsub_topic" "app-route-metrics-topic" {
   name         = "route-metrics-topic"
   kms_key_name = google_kms_crypto_key.route-metrics-crypto-key.id
+
+  depends_on = [google_pubsub_schema.app-route-metrics-topic-schema]
+  schema_settings {
+    schema   = google_pubsub_schema.app-route-metrics-topic-schema.id
+    encoding = "JSON"
+  }
+}
+
+
+resource "google_pubsub_topic" "app-route-dead-letter-topic" {
+  name = "route-dead-letter-topic"
 }
 
 resource "google_pubsub_subscription" "app-route-metrics-subscription" {
@@ -8,7 +47,14 @@ resource "google_pubsub_subscription" "app-route-metrics-subscription" {
   topic = google_pubsub_topic.app-route-metrics-topic.id
 
   bigquery_config {
-    table = "${google_bigquery_table.route-metrics.project}.${google_bigquery_table.route-metrics.dataset_id}.${google_bigquery_table.route-metrics.table_id}"
+    table               = "${google_bigquery_table.route-metrics.project}.${google_bigquery_table.route-metrics.dataset_id}.${google_bigquery_table.route-metrics.table_id}"
+    use_topic_schema    = true
+    drop_unknown_fields = true
+  }
+
+  dead_letter_policy {
+    dead_letter_topic     = google_pubsub_topic.app-route-dead-letter-topic.id
+    max_delivery_attempts = 10
   }
 }
 
